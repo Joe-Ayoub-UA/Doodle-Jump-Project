22afb606acd1a6587db86190bba3a6e99a0d7daa
joeayoub0 <s0230501@ad.ua.ac.be>
2024-11-05 15:19:59 +0100

    push1 yml gemaakt main en CMake werkend

---
+++ README.md
@@ -0,0 +1,5 @@
+Project Advanced Programming 2023-2024
+=======================================
+
+Name: Joe Ayoub    
+Student number: 20230501
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by s0230501 on 5/11/24.
+//
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFMLApplication");
+    sf::CircleShape shape;
+    shape.setRadius(40.f);
+    shape.setPosition(100.f, 100.f);
+    shape.setFillColor(sf::Color::Cyan);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
